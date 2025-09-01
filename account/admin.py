44abc29f4b3a237197583f7a10bcc43e23f55f2c
Mazharul Islam <abracadabraamm@gmail.com>
2025-09-01 15:40:34 +0600

    registered profile model in admin interface

--- account/admin.py
+++ account/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from . import models
 
 # Register your models here.
+
 @admin.register(models.User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ["email", "name", "role", "active_status", "date_joined"]
@@ -22,3 +23,22 @@ class UserAdmin(admin.ModelAdmin):
         return user.is_active
     active_status.boolean = True
     active_status.short_description = "Active"
+
+
+
+@admin.register(models.Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ["user_email", "user_name", "phone", "gender", "default_address", "created_at"]
+    list_select_related = ["user", "default_address"]
+    list_per_page = 20
+    list_filter = ["gender", "created_at"]
+    search_fields = ["user__email", "phone", "user__first_name", "user__last_name"]
+    ordering = ["created_at"]
+
+    @admin.display(ordering="user__first_name", description="Name")
+    def user_name(self, profile):
+        return f"{profile.user.first_name} {profile.user.last_name}".strip()
+
+    @admin.display(ordering="user__email", description="Email")
+    def user_email(self, profile):
+        return profile.user.email
