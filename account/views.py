8ba270a17eae92cc88fe270cf5d38196561bef1e
Mazharul Islam <abracadabraamm@gmail.com>
2025-09-03 03:24:05 +0600

    Class Based RegisterView Written

--- account/views.py
+++ account/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import UserSerializer
 
-# Create your views here.
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(
+                {
+                    "message": "User registered successfully", 
+                    "user": serializer.data
+                },
+                status=status.HTTP_201_CREATED
+            )
