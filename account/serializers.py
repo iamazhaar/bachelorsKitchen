73a7feedd1dbff0bb75d2386af70a7deefad099f
Mazharul Islam <abracadabraamm@gmail.com>
2025-09-03 03:25:17 +0600

    create() overridden to store hashed password

--- account/serializers.py
+++ account/serializers.py
@@ -7,5 +7,12 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ["id", "email", "first_name", "last_name", "password", "role"]
         extra_kwargs = {
             "password": {"write_only": True},
-            "role": {"read_only": True}
+            "role": {"read_only": True},
         }
+    
+    def create(self, validated_data):
+        password = validated_data.pop("password")
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
